60492a7e7df041633e16f3d4c2adc58131f2c2cc
Chad Franzen <chadfranzen@Chad-Franzen-Intern-MacBook-Pro.local>
2015-08-21 11:56:59 -0500

    Start router work

--- public_html/entry.js
+++ public_html/entry.js
@@ -1,1 +1,7 @@
-document.write("Hello");
+import sectionRouter from "./router";
+
+$(document).ready(() => {
+	$('.menu').click((ev) => {
+		$('#nav').toggleClass('active');
+	});
+});
---
+++ public_html/router.js
@@ -0,0 +1,17 @@
+var SectionRouter = Backbone.Router.extend({
+	routes: {
+		"help": "help"
+	},
+
+	initialize: function() {
+		debugger;
+	},
+
+	help: function() {
+		debugger;
+	}
+});
+
+var sectionRouter = new SectionRouter();
+
+export default sectionRouter;
