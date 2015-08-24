3ca7bd70fb36e0264309e3340685f9394639c843
cdfranz2 <cdfranz2@illinois.edu>
2015-08-24 18:49:50 -0500

    Contact Page

--- public_html/script/about.js
+++ public_html/script/about.js
@@ -4,11 +4,11 @@ var About = React.createClass({
 	render: function(){
 		return (
 			<div>
-				<span className="section-header">Projects</span>
 				<div className="underline" />
-				<div>Hi, my name is Chad Franzen. I am currently a Computer Science major at the University of Illinois at Urbana-Champaign but I'm originally from Algonquin, IL.</div>
-				<a>Try It!</a>
-				<a>Github</a>
+				<p>Hi, my name is Chad Franzen. I am currently a Computer Science major at the University of Illinois at Urbana-Champaign but I'm originally from Algonquin, IL.</p>
+				<p>In high school, I competed in Worldwide Youth in Science and Engineering and won medals at the state finals. On campus, I'm a part of the Association for Computing Machinery and the Campus Honors Program. In elementary school I once earned a gold star on my multiplication test.</p>
+				<p>I've had experience working in the logistics department of an energy management company, as well as lots of volunteer experience in tutoring and other areas.</p>
+				<p>My hobbies include video games, reading, listening to standup comedy, and bugging people about A Song of Ice and Fire.</p>
 			</div>
 		);
 	}
